f7dd639a88e7958ce90e92743e107ea234755b2b
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-02 17:03:05 +0530

    added styles for problem statements card

--- components/PScard.jsx
+++ components/PScard.jsx
@@ -11,15 +11,16 @@ import {
     off,
 } from "firebase/database";
 
-function ProblemStatement({ Id, statement }) {
+function ProblemStatement({ Id, statement, description }) {
     const [count, setCount] = useState(0);
     const [selected, setSelected] = useState(false);
     const TEAM_ID = 1;
+    const [isConfirmationModalOpen, setConfirmationModalOpen] = useState(false);
 
     useEffect(() => {
         console.log(Id);
         console.log(1);
-        const problemRef = ref(database, `ProblemStatements/1`);
+        const problemRef = ref(database, `ProblemStatements/${Id}`);
         console.log(2);
         console.log(problemRef);
         onValue(problemRef, (snapshot) => {
@@ -34,11 +35,15 @@ function ProblemStatement({ Id, statement }) {
 
         return () => {
             console.log(4);
-            // off(problemRef);
+            off(problemRef);
         };
     }, [Id, statement]);
 
     const handleSelect = () => {
+        setConfirmationModalOpen(true); // Open the confirmation modal
+    };
+
+    const confirmSelection = () => {
         console.log(5);
         const problemRef = ref(database, `ProblemStatements/${id}`);
         const transaction = (problem) => {
@@ -61,13 +66,75 @@ function ProblemStatement({ Id, statement }) {
     };
 
     return (
-        <div>
-            <h2>{statement}</h2>
-            <p>Selected by {count} teams</p>
+        <div
+            className={`${
+                count >= 5 ? "cursor-not-allowed blur-[1px]" : "cursor-default"
+            } flex flex-col w-5/6 gap-1 p-6 mx-auto shadow-md outline-border bg-none rounded-3xl relative`}
+        >
+            <div className={" flex gap-4 font-bold md:text-xl text-bblue-200 "}>
+                <span className=" text-bgold-200">{Id}</span>
+                <span>{statement}</span>
+            </div>
+
+            <p className="justify-center text-left md:text-xl text-slate-200">
+                Status: ({count}/5){" "}
+                <span
+                    className={`${
+                        count >= 5 ? "inline " : "hidden"
+                    } text-bred-200 visible-text`}
+                >
+                    {" "}
+                    Max selections reached
+                </span>
+            </p>
+            <p className="text-xs leading-5 md:text-base">
+                <span className="text-base md:text-xl text-bblue-200">
+                    Description:{" "}
+                </span>
+                {description}
+            </p>
             {selected ? (
                 <p>Selected</p>
             ) : (
-                <button onClick={handleSelect}>Select</button>
+                <>
+                    <button
+                        onClick={handleSelect}
+                        className="px-4 py-2 mx-auto mt-2 text-xl font-bold duration-300 border rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:hover:text-bblue-200 w-min text-bblue-200 border-bgold-200 hover:text-bgold-200"
+                        disabled={count >= 5}
+                    >
+                        Select
+                    </button>
+                    {isConfirmationModalOpen && (
+                        <div className="fixed inset-0 z-50 flex items-center justify-center bg-Manga-100 bg-opacity-5 backdrop-blur">
+                            <div className="flex flex-col w-1/2 gap-2 p-4 bg-white rounded-lg shadow-lg modal-container glass">
+                                <p className="text-xl text-bblue-200">
+                                    Are you sure you want to select the problem
+                                    statement{" "}
+                                    <span className="text-2xl font-bold text-bgold-200">
+                                        {Id}
+                                    </span>
+                                    ?
+                                </p>
+                                <div className="flex justify-center gap-2">
+                                    <button
+                                        onClick={() =>
+                                            setConfirmationModalOpen(false)
+                                        }
+                                        className="px-4 py-2 my-2 text-base font-bold duration-300 border rounded-lg cursor-pointer text-bred-200 border-bgold-200 hover:text-bgold-200"
+                                    >
+                                        No
+                                    </button>
+                                    <button
+                                        onClick={confirmSelection}
+                                        className="px-4 py-2 my-2 text-base font-bold duration-300 border rounded-lg cursor-pointer text-void border-bgold-200 bg-bblue-200 hover:bg-bblue-100"
+                                    >
+                                        Yes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
--- components/Statements.jsx
+++ components/Statements.jsx
@@ -7,23 +7,30 @@ const TEAM_ID = 1; // Replace with the actual team ID
 const problemStatements = [
     {
         Id: "PS1",
-        statement: "Problem Statement 1",
+        statement: "Healthcare Practice Management System.",
+        description:
+            "Create a mobile app or web app that facilitates user registration and login, enabling users to access and manage their profiles. It offers features such as appointment scheduling, secure file uploads and viewing, comprehensive treatment planning, cost estimation, and seamless billing generation for efficient healthcare practice management ",
     },
     {
         Id: "PS2",
-        statement: "Problem Statement 2",
+        statement: "Hostel Management System",
+        description:
+            "Create a mobile app or web app that facilitates user registration and login, enabling users to access and manage their profiles. It offers features such as appointment scheduling, secure file uploads and viewing, comprehensive treatment planning, cost estimation, and seamless billing generation for efficient healthcare practice management ",
     },
     // Add more problem statements...
 ];
 
 function Statements() {
     return (
-        <div>
-            <h1>Problem Statement Selection</h1>
+        <div className="flex flex-col min-h-screen gap-8">
+            <h1 className="mx-auto mt-8 text-xl md:text-3xl shimmerb">
+                Problem Statement Selection
+            </h1>
             {problemStatements.map((problem) => (
                 <ProblemStatement
                     key={problem.Id}
                     Id={problem.Id}
+                    description={problem.description}
                     statement={problem.statement}
                 />
             ))}
