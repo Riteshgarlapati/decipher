2e7d267f53dff5db97f18b8ad181fdfaf73b9ddb
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-07 03:03:56 +0530

    Increased PS limit

--- components/PScard.jsx
+++ components/PScard.jsx
@@ -31,7 +31,7 @@ function ProblemStatement({ Id, statement, description, onSelect }) {
         <div className="relative z-0 flex justify-center">
             <div
                 className={` ${
-                    count >= 5 ? "inline " : "hidden"
+                    count >= 6 ? "inline " : "hidden"
                 } text-bred-200 text-xl md:text-2xl font-bold z-10 backdrop-blur-[1px] cursor-not-allowed absolute  w-5/6 flex items-center justify-center  h-full`}
             >
                 <span className="px-4 py-2 border shadow-md rounded-xl bg-void border-bred-200">
@@ -54,7 +54,7 @@ function ProblemStatement({ Id, statement, description, onSelect }) {
                 </div>
 
                 <p className="justify-center text-left md:text-xl text-slate-200">
-                    Status: ({count}/5){" "}
+                    Status: ({count}/6){" "}
                 </p>
                 <p className="text-xs leading-5 md:text-base">
                     <span className="text-base md:text-xl text-bblue-200">
@@ -66,7 +66,7 @@ function ProblemStatement({ Id, statement, description, onSelect }) {
                 <button
                     onClick={handleSelect}
                     className="px-4 py-2 mx-auto mt-2 text-xl font-bold duration-300 border rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:hover:text-bblue-200 w-min text-bblue-200 border-bgold-200 hover:text-bgold-200"
-                    disabled={count >= 5}
+                    disabled={count >= 6}
                 >
                     Select
                 </button>
